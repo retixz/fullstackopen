c641978e6d00e656b34427bd26ca6ee1e7d58144
retixz <alexunderq@gmail.com>
2021-05-25 15:52:58 +0300

    1.3- 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,26 +4,29 @@ import Total from "./Total.js";
 import Part from "./Part.js";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Part course={course} />
+      <Total course={course} />
     </div>
   );
 };
--- part1/src/Total.js
+++ part1/src/Total.js
@@ -3,8 +3,10 @@ import React from "react";
 const Total = (props) => {
   return (
     <p>
-      Number of exercises
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      Number of exercises{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
     </p>
   );
 };
