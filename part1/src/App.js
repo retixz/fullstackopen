976f72d52487dc4d643e72864eb8b7b0c07d2492
retixz <alexunderq@gmail.com>
2021-05-26 11:10:01 +0300

    unicafe 1.6 - 1.11*

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,60 @@
-import React from "react";
-import Header from "./Header.js";
-import Total from "./Total.js";
-import Part from "./Part.js";
+import React, { useState } from "react";
+
+const Button = ({ setHandler, name }) => {
+  return <button onClick={setHandler}> {name}</button>;
+};
+
+const Statistic = ({ text, value }) => {
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
+  );
+};
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header course={course} />
-      <Part course={course} />
-      <Total course={course} />
-    </div>
-  );
+  const total = good + neutral + bad;
+  const average_score = (good * 1 + neutral * 0 + bad * -1) / total;
+  const positive_percentage = (good / total) * 100;
+
+  if (total > 0)
+    return (
+      <>
+        <h2>Give feedback</h2>
+        <Button setHandler={() => setGood(good + 1)} name="good" />
+        <Button setHandler={() => setNeutral(neutral + 1)} name="neutral" />
+        <Button setHandler={() => setBad(bad + 1)} name="bad" />
+        <h2>statistics</h2>
+        <table>
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="total" value={total} />
+            <Statistic text="average" value={average_score} />
+            <Statistic text="positive" value={positive_percentage + " %"} />
+          </tbody>
+        </table>
+      </>
+    );
+  else
+    return (
+      <>
+        <h2>Give feedback</h2>
+        <Button setHandler={() => setGood(good + 1)} name="good" />
+        <Button setHandler={() => setNeutral(neutral + 1)} name="neutral" />
+        <Button setHandler={() => setBad(bad + 1)} name="bad" />
+        <p>No feedback given</p>
+      </>
+    );
 };
 
 export default App;
