6693029eb68f97d00c78dea6035dabe4c9dae328
retixz <alexunderq@gmail.com>
2021-05-31 16:30:29 +0300

    part3

---
+++ part3/backend/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+morgan.token("body", (request) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Cosmin",
+    number: "444444444",
+    id: 4,
+  },
+  {
+    name: "cr",
+    number: "34",
+    id: 5,
+  },
+  {
+    name: "conecter",
+    number: "3423342",
+    id: 6,
+  },
+  {
+    name: "Gica",
+    number: "99999",
+    id: 7,
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  obj = {
+    info: `Phonebook has info for ${persons.length} people`,
+    time: new Date(),
+  };
+
+  response.send(`<div>${obj.info}</div> <div>${obj.time}</div>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  } else if (persons.find((person) => body.name === person.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000),
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
